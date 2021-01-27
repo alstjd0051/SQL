9ea732958439f3bbefe9f9f7d5b9b0f8ce9a1455
dev_addictied <70365399+alstjd0051@users.noreply.github.com>
2021-01-27 22:43:24 +0900

    Add files via upload

--- oracle_workspace/oracle_chun.sql
+++ oracle_workspace/oracle_chun.sql
@@ -42,7 +42,8 @@ where category = '인문사회';
 --
 --63 rows selected
 -----------------------------------------------------------------------------
-select distinct department_name ||'의 정원은 ' ||capacity ||'명 입니다.'
+--distinct 칼럼의 중복되는 값을 제거 후 보여준다.
+select distinct department_name ||'의 정원은 ' ||capacity ||'명 입니다.' "학과별 정원"
 from tb_department;
 
 
@@ -162,3 +163,118 @@ where extract(year from entrance_date) = 2002
         and absence_yn = 'N' 
         and instr(student_address, '전주') = 1;
 --        where absence_yn = 'N' and extract(year from entrance_date) = 2002 and student_address like '전주%';
+
+
+--1. 학과테이블에서 계열별 정원의 평균을 조회(정원 내림차순 정렬)
+select category, trunc(avg(capacity),0)capacity
+from tb_department
+group by category
+order by capacity;
+
+---------------------강사---------------------
+select category,
+            trunc(avg(capacity))
+from tb_department
+group by category
+order by 2 desc;
+
+--2. 휴학생을 제외하고, 학과별로 학생수를 조회(학과별 인원수 내림차순)
+select department_no 학과, count(department_no) 학과별인원수
+from tb_student
+where absence_yn = 'Y'
+group by department_no
+order by count(department_no);
+
+---------------------강사---------------------
+select department_no, count(*)
+from tb_student
+where absence_yn = 'N'
+group by department_no
+order by 1 desc;
+
+--3. 과목별 지정된 교수가 2명이상이 과목번호와 교수인원수를 조회
+select class_no, count(professor_no)
+from tb_class_professor
+group by class_no
+having count(class_no) >= 2;
+
+---------------------강사---------------------
+select class_no, count(*)
+from tb_class_professor
+group by class_no
+having count(*) >=2
+order by 2 desc;
+--4. 학과별로 과목을 구분했을때, 과목구분이 "전공선택"에 한하여 과목수가 10개 이상인 행의 학과번호, 과목구분(class_type), 과목수를 조회(전공선택만 조회)
+
+
+---------------------강사---------------------
+select department_no ,
+                class_type,
+                count(*)
+from tb_class
+group by department_no , class_type
+having class_type = '전공선택' and count(*)>= 10
+order by 3 desc;
+
+
+--1. 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학 년도를 입학 년도가 빠른
+--순으로 표시하는 SQL 문장을 작성하시오.( 단, 헤더는 "학번", "이름", "입학년도" 가
+--표시되도록 한다.)
+/*
+학번              이름                          입학년도
+----------    -------------------- -----------
+9973003  김용근귺                 1999-03-01
+A473015  배용원                     2004-03-01
+A517105  이신열                     2005-03-01
+*/
+
+SELECT student_no 학번, student_name 이름, to_char(entrance_date, 'yyyy-mm-dd') 입학년도
+from tb_student
+where department_no  = '002'
+order by entrance_date asc;
+
+--2. 춘 기술대학교의 교수 중 이름이 세 글자가 아닌 교수가 한 명 있다고 한다. 그 교수의
+--이름과 주민번호를 화면에 출력하는 SQL 문장을 작성해 보자. (* 이때 올바르게 작성핚 SQL 
+--문장의 결과 값이 예상과 다르게 나올 수 있다. 원인이 무엇일지 생각해볼 것)
+/*
+PROFESSOR_NAME        PROFESSOR_SSN
+--------------------           ----------------
+강혁                                      601004-1100528
+박강아름                              681201-2134896
+*/
+
+select professor_name,professor_ssn
+from tb_professor
+where length(professor_name)!=3;
+
+--3. 춘 기술대학교의 남자 교수들의 이름과 나이를 출력하는 SQL 문장을 작성하시오. 단
+--이때 나이가 적은 사람에서 많은 사람 순서로 화면에 출력되도록 만드시오. (단, 교수 중
+--2000 년 이후 출생자는 없으며 출력 헤더는 "교수이름", "나이"로 한다. 나이는 ‘만’으로
+--계산한다.)
+/*
+교수이름                     나이
+-------------------- ----------
+제상철                         28
+주영상                         28
+김명석                         30
+신영호                         30
+박지평                         32
+…
+75 rows selected
+*/
+/*
+--나이 추출시 주의점
+--현재년도 - 탄생년도 + 1 => 한국식 나이
+--select emp_name,
+--            emp_no,
+--            substr(emp_no, 1, 2),
+----            extract(year from to_date(substr(emp_no, 1, 2), 'yy')),
+----           extract(year from sysdate) -  extract(year from to_date(substr(emp_no, 1, 2), 'yy')) + 1,
+----           extract(year from to_date(substr(emp_no, 1, 2), 'rr')),
+----           extract(year from sysdate) -  extract(year from to_date(substr(emp_no, 1, 2), 'rr')) + 1
+--            extract(year from sysdate) -
+--            (decode(substr(emp_no, 8, 1), '1', 1900, '2', 1900, 2000) + substr(emp_no, 1, 2)) + 1 age
+--from employee;*/
+select professor_name,professor_ssn
+from tb_professor;
+               
